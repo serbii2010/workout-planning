4efd3dbec3f672a8f1fb63efb885fdd9ac18b755
Кирносов Александр <serbii2010@gmail.com>
2022-09-08 13:30:37 +0600

    Initial commit

---
+++ src/main/java/com/thumbtack/school/workoutplanning/WorkoutPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.thumbtack.school.workoutplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlanningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkoutPlanningApplication.class, args);
+    }
+
+}
